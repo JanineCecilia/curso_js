7a51ac5f90ecaa4eed4b148ee2178eabe12a1072
JanineCecilia <janine.cecilia@hotmail.com>
2021-01-13 17:09:22 -0300

    Adicionando os exercicios 110 010 e pais a pasta da aula 11

---
+++ cursojs/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){
+    console.log('Sua velocidade ultrapassou o limite de 60km/h. MULTADO!')
+}
+console.log('Dirija sempre com responsa!')
